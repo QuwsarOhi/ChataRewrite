09342b88716104bbe3595cd4331cf2a125b2f251
Quwsar Ohi <quwsarohi@gmail.com>
2024-08-24 17:27:10 -0600

    finetune v0

---
+++ finetune.py
@@ -0,0 +1,207 @@
+# %%
+import torch
+import copy
+import datasets
+
+from peft import (
+    LoftQConfig,
+    LoraConfig,
+    TaskType,
+    get_peft_model,
+    PeftModel
+)
+
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    AutoConfig,
+    BitsAndBytesConfig,
+    StoppingCriteria,
+    StoppingCriteriaList,
+    DataCollatorForSeq2Seq,
+    TrainingArguments,
+    Trainer
+)
+
+# %%
+from dataclasses import dataclass
+@dataclass
+class DataClass:
+    MODEL_PATH = "Qwen/Qwen2-0.5B-Instruct"      # Qwen/Qwen2-0.5B
+    MAX_LENGTH = 64
+    EPOCH = 1
+    LORA_RANK = 2
+    LORA_ALPHA = 2 * LORA_RANK
+    LORA_DROPOUT = 0.3
+    LORA_MODULES = ["o_proj", "qjv_proj", "gate_up_proj"]
+    LR = 5e-5
+    MODEL_SAVE_FOLDER = 'weights'
+
+# %%
+model_config = AutoConfig.from_pretrained(
+    DataClass.MODEL_PATH,
+    trust_remote_code = True,
+    attn_implementation = 'eager', #'flash_attention_2'
+)
+
+tokenizer = AutoTokenizer.from_pretrained(
+    DataClass.MODEL_PATH,
+    trust_remote_code = True
+)
+
+tokenizer.pad_token = tokenizer.eos_token
+
+# quant_config = BitsAndBytesConfig(
+#     load_in_4bit = False,
+#     bnb_4bit_quant_type="n4f",
+#     bnb4bit_compute_dtype=torch.bfloat16,
+#     bnb_4bit_use_double_quant=True
+# )
+
+model = AutoModelForCausalLM.from_pretrained(
+    DataClass.MODEL_PATH,
+    device_map='mps',
+    low_cpu_mem_usage=True,
+    # load_in_8bit=True,
+    # load_in_4bit=True,
+    attn_implementation='eager', #'flash_attention_2',
+    torch_dtype=torch.bfloat16, # NOTE: MPS does not support torch.bfloat16 finetuning
+    trust_remote_code=True,
+    # quantization_config=quant_config
+)
+
+# %%
+import os
+
+# Macbook MPS
+os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = '1'
+
+def inference(input_text):
+    input_ids = tokenizer(input_text, return_tensors="pt")
+    # print(input_ids.keys())
+    outputs = model.generate(
+        # **input_ids
+        max_new_tokens=100,
+        do_sample=False,
+        num_beams=1,
+        temperature=None,
+        top_k=None,
+        top_p=None,
+        input_ids=input_ids['input_ids'].to('mps'),
+        attention_mask=input_ids['attention_mask'].to('mps')
+    )
+    return tokenizer.decode(outputs[0])
+
+# %%
+input_text = "Write a poem in machine learning.\n"
+print(inference(input_text=input_text))
+
+# %%
+# Let's see the chat template
+
+prompt = "Write a poem in machine learning."
+messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": prompt}
+]
+
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
+
+print("Chat Template:")
+print(text)
+print('-----')
+print("Output:")
+print(inference(text))
+
+# %%
+from datasets import load_dataset
+
+train_dataset = load_dataset("json", data_files="train.json")
+test_dataset = load_dataset("json", data_files="valid.json")
+
+# %%
+print(train_dataset)
+print(test_dataset)
+
+# %%
+def prompt_formatter(data):
+    """Formatting the prompt"""
+    data = \
+f'''<|im_start|>system
+You are an advanced language model adept at interpreting and refining noisy or imperfect user inputs. 
+Given user data, your task is to accurately extract the intended question and provide precise answers or predictions, even if the input contains errors or discontinuities.<|im_end|>
+<|im_start|>user
+{data['input_disfluent']}<|im_end|>
+<|im_start|>assistant
+{data['output_original']}<|im_end|>'''
+
+    return {'sentence': data, 'input_ids': '', 'attention_mask': '', 'labels': ''}
+
+print(prompt_formatter(train_dataset['train'][0])['sentence'])
+
+# %%
+def batch_tokenizer(batch):
+    """Tokenization of data"""
+    model_inputs = tokenizer(
+        batch["sentence"],
+        max_length=DataClass.MAX_LENGTH,
+        truncation=True,
+        padding='max_length'
+    )
+    # HF automatically performs right shift
+    model_inputs['labels'] = copy.deepcopy(model_inputs['input_ids'])
+    return model_inputs
+
+# %%
+train_dataset = train_dataset.map(prompt_formatter)
+train_dataset = train_dataset.map(batch_tokenizer, batched=True, remove_columns = [])
+test_dataset = test_dataset.map(prompt_formatter)
+test_dataset = test_dataset.map(batch_tokenizer, batched=True, remove_columns = [])
+
+# %%
+data_collator = DataCollatorForSeq2Seq(
+    model = model,
+    tokenizer = tokenizer,
+    max_length = DataClass.MAX_LENGTH,
+    pad_to_multiple_of = 8,
+    padding = 'max_length'
+)
+
+training_args = TrainingArguments(
+    disable_tqdm=True,
+    output_dir = DataClass.MODEL_SAVE_FOLDER,
+    overwrite_output_dir=True,
+    # fp16=True,
+    # turncation=True,
+    per_device_eval_batch_size=1,
+    learning_rate=DataClass.LR,
+    num_train_epochs=DataClass.EPOCH,
+    logging_strategy='epoch',
+    eval_strategy='epoch',
+    save_strategy='epoch',
+    push_to_hub=False,
+    weight_decay=0.9,
+    report_to=[]
+)
+
+# %%
+trainer = Trainer(
+    model = model,
+    tokenizer= tokenizer,
+    args = training_args,
+    train_dataset= train_dataset['train'],
+    eval_dataset=test_dataset['train'],
+    data_collator=data_collator
+)
+
+model.config.use_cache = False
+trainer.train()
+
+# %%
+
+
+
